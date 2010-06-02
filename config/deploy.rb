62f9ef04e728908c336e05279e790d782ccedcc8
yfl <miderph@miderph-laptop.(none)>
2010-06-03 00:32:37 +0800

    update spin

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
-set :application, "test_app"
-role :app, application
-role :web, application
-role :db,  application, :primary => true
+set :application, "depot"
+role :app, "192.168.1.109"
+role :web, "192.168.1.109"
+role :db,  "192.168.1.109", :primary => true
 
 set :user, "miderph"
 set :password,"carrier"
@@ -10,6 +10,13 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
-set :repository, "git://github.com/ryanb/railscasts.git"
+set :repository, "git://github.com/miderph/depot.git"
 set :branch, "master"
 
+task :chmod ,:roles=>:web do
+  run "chmod -fR 755 #{deploy_to}/current/script/*"
+end
+
+after "deploy:symlink",:chmod
+
+#default_environment["PATH"] ="/usr/local/ruby/bin:/usr/local/imageMagick/bin:/usr/lib/jvm/java-6-sun-1.6.0.14/bin:/usr/lib/jvm/java-6-sun-1.6.0.14/jre/bin:/usr/local/ruby/bin:/usr/local/ruby/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games"
